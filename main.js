5a2ce4fc9b9233b02c363892b30394f21c2d7a29
YanRafaelW <wuicik.yan@escola.pr.gov.br>
2024-09-26 11:49:12 -0300

    hhgk

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let idade = promopt ('qual a idade');
+document.write idade;
+
 function soma (num1, num2){
     return num1 + num2;
 }
@@ -7,3 +10,4 @@ function areaQuadrado (lado){
     return lado ** 2;
 }
 console.log (areaQuadrado(6));
+
